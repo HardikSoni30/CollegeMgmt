3703fba28d017e9b6edf0da5b083cec2b58e4699
HardikSoni30 <hardiknsoni30@gmail.com>
2020-10-05 01:52:47 +0530

    updated

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,6 @@
-from django.contrib.auth.views import LogoutView
+from django.contrib.auth.views import (LogoutView,
+                                       PasswordChangeView,
+                                       PasswordChangeDoneView)
 from django.urls import path
 from .views import (SignUpView, SignInView,
                     StaffProfileView, StudentProfileView)
@@ -8,6 +10,8 @@ app_name = 'accounts'
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
     path('login/', SignInView.as_view(), name='login'),
+    path('change-password/', PasswordChangeView.as_view(template_name='password_change_form.html'), name='change_password'),
+    path('password_change/done/', PasswordChangeDoneView.as_view(template_name='password_change_done.html'), name='change_password_done'),
     path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
     path('profile/<int:user_id>/', StaffProfileView.as_view(), name='profile_staff'),
     path('profile-student/<int:user_id>/', StudentProfileView.as_view(), name='profile_student'),
